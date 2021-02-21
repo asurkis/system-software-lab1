317df2aae84580ddefa9d535a99e3316b9896768
Anton Surkis <antonsurkis@gmail.com>
2021-02-21 16:53:52 +0300

    first commit

---
+++ Xfs.h
@@ -0,0 +1,4 @@
+#ifndef _XFS_H_
+#define _XFS_H_
+
+#endif
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "Xfs.h"
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  for (int i = 0; i < argc; ++i) {
+
+  }
+  return 0;
+}
